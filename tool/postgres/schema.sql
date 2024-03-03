2ea786739c98eefe3336c542c149ad2037476fa7
wwwshwww <www.shinderu.www@gmail.com>
2024-03-03 23:14:13 +0900

    enrich operations for auto migration

--- tool/postgres/schema.sql
+++ tool/postgres/schema.sql
@@ -1,10 +1,10 @@
--- sqldef の差分出力機能の動作確認のためだけに存在するダミーのスキーマ定義
 
 CREATE TABLE users (
     id bigint NOT NULL PRIMARY KEY,
     name text,
     age int
 );
+CREATE INDEX index_name ON users(name);
 
 CREATE TABLE bigdata (
     data bigint

37d371ee44815cf863a9ffbbf584961c2d809eec
wwwshwww <www.shinderu.www@gmail.com>
2024-03-08 01:35:55 +0900

    add swag

--- sample_auth_app/adapter/inbound/echo/user_service/service.go
+++ sample_auth_app/adapter/inbound/echo/user_service/service.go
@@ -3,6 +3,7 @@ package user_service
 import (
 	"example_app/sample_auth_app/common/mycontext"
 	"example_app/sample_auth_app/usecase/user_authentication"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,10 +18,24 @@ func New(newUserAuthenticationUsecase func(ctx mycontext.Context) user_authentic
 	}
 }
 
+// @Summary CreateNewUser
+// @Description you don't need session's id and token when request
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Success 204 "" ""
+// @Router /user/signup [post]
 func (s service) SignUp(c echo.Context) error {
-	// uu := s.newUserAuthenticationUsecase(mycontext.From(c.Request().Context()))
+	uu := s.newUserAuthenticationUsecase(mycontext.From(c.Request().Context()))
 
-	// qp := c.QueryParams()
-	// var name, password string
-	return nil
+	var signUpInput SignUpInput
+	if err := c.Bind(&signUpInput); err != nil {
+		return err
+		// return c.String(http.StatusBadRequest, "invalid param")
+	}
+
+	if err := uu.SignUp(UnmarshalSignUpInput(signUpInput)); err != nil {
+		return err
+	}
+	return c.String(http.StatusNoContent, "")
 }
--- sample_auth_app/main.go
+++ sample_auth_app/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"example_app/sample_auth_app/adapter/inbound/echo/user_service"
+	"example_app/sample_auth_app/common/mycontext"
+	"example_app/sample_auth_app/domain/session/session"
+	"example_app/sample_auth_app/domain/user/user"
+	"example_app/sample_auth_app/domain/user/user_finder"
+	"example_app/sample_auth_app/domain_service/secure_hasher"
+	"example_app/sample_auth_app/domain_service/user_validation_service"
+	"example_app/sample_auth_app/usecase/user_authentication"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
+// @title Sample App API
+// @version 1.0
+// @description This is a sample server for Echo framework.
+// @host localhost:1323
+// @BasePath /api/v1
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello.\n")
 	})
+
+	userService := user_service.New(func(ctx mycontext.Context) user_authentication.Usecase {
+		return user_authentication.New(
+			ctx,
+			&user.RepositoryMock{},
+			&user_finder.FinderMock{},
+			&session.RepositoryMock{},
+			user_validation_service.New(
+				&user_validation_service.PortMock{},
+			),
+			secure_hasher.New(),
+		)
+	})
+
+	e.POST("/user/signup", userService.SignUp)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
